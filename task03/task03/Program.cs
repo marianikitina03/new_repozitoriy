bda3fe0b4863841d2fdcd013e1d689f79917978c
maria <makwa4923@gmail.com>
2024-10-07 00:42:17 +0500

    Задание 3

---
+++ task03/task03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число n");
+            var number = int.Parse(Console.ReadLine());
+
+            var first = number / 100 ;
+            var second = number / 10 % 10;
+            var third = number % 10;
+
+            Console.WriteLine("Исходное число x = "+ second+ ""+ first + "" + third);
+            Console.ReadKey();
+
+        }
+    }
+}
