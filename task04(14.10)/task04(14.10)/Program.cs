93d58a70d081292bc3feca1073267ca4e902539c
maria <makwa4923@gmail.com>
2024-10-13 14:50:39 +0500

    ЗАДАНИЕ 4 выполнено. Вычисление по формуле.

---
+++ task04(14.10)/task04(14.10)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task04_14._10_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите действительное число х");
+            var x = double.Parse(Console.ReadLine());
+
+            var y = MyFunction(x);
+            Console.WriteLine($"f(x)={y}");
+            Console.ReadKey();
+
+
+        }
+
+        static double MyFunction(double x)
+        {
+            return Math.Sqrt((x * x) + (Math.Sqrt((x * x) + (1 / (x * x)))));
+        }
+    }
+    
+}
