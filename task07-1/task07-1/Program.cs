e6037416a4598cdbf21b81ed6fa66d098bd15a11
maria <makwa4923@gmail.com>
2024-10-21 16:51:22 +0500

    ЗАДАНИЕ 7.1 выполнено. Проверка утверждения на истинность.

---
+++ task07-1/task07-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task07_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = GetNumber("m");
+            var n = GetNumber("n");
+
+            if (IsStatementTrue(m, n))
+                Console.WriteLine("Утверждение истинно");
+            else
+                Console.WriteLine("Утверждение ложно");
+
+            Console.ReadKey();
+        }
+
+
+        static bool IsStatementTrue(int m, int n)
+        {
+            return m > 10 && n > 10;
+        }
+
+        static int GetNumber(string numberName)
+        {
+            Console.WriteLine($"Введите число {numberName}");
+            
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
