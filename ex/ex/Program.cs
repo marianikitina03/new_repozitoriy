7e20c3cf55325e2adc1df07f687d664e8df3ecef
maria <makwa4923@gmail.com>
2024-12-09 09:47:18 +0500

    ЗАДАНИЕ 10.1 выполнено

---
+++ ex/ex/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Person mike;
+            mike = new Person();
+            mike.Name = "Михаил";
+            mike.LastName = "Иванов";
+            mike.SetAge(20);
+
+            var kate = new Person()
+            {
+                Name = "Екатерина",
+                LastName = "Петрова",
+                kate.SetAge(19)
+            };
+
+            mike.PrintInfo();
+            kate.PrintInfo();
+            Console.ReadKey();
+        }
+        public class Person
+        {
+            public string Name;
+            public string LastName;
+            private int age;
+
+            public void SetAge(int age)
+            {
+                if (age < 0 || age > 150)
+                    throw new ArgumentException();
+                this.age = age;
+            }
+            public void PrintInfo()
+            {
+                Console.WriteLine($"{Name} {LastName}, возраст: {Age}");
+            }
+        }
+       
+
+    }
+    
+}
+
+
---
+++ task10.1/task10.1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            if (!TryInputNumber("Введите число n", out n))
+            {
+                Console.ReadKey();
+                return;
+            }
+
+            if (n <= 0)
+            {
+                Console.WriteLine("Число n должно быть больше 0");
+                Console.ReadKey();
+                return;
+            }
+
+            double sum = 0;
+
+            for (var i = 1; i <= n; i++)
+            {
+                sum += Math.Sqrt(i);
+            }
+
+            Console.WriteLine($"Сумма квадратных корней всех целых чисел от 1 до {n} равна {sum}");
+            Console.ReadKey();
+        }
+
+        static bool TryInputNumber(string message, out int number)
+        {
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+
+            if (!int.TryParse(input, out number))
+            {
+                Console.WriteLine("Ошибка ввода");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
