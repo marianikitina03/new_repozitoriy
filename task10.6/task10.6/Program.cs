cdb6f5bce2a6972034e85f5d850d8b804ce05326
maria <makwa4923@gmail.com>
2024-12-09 10:39:42 +0500

    ЗАДАНИЕ 10.6 выполнено

---
+++ task10.6/task10.6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Трехзначные числа, в которых нет одинаковых цифр:");
+
+            string result = "";
+
+            for (int number = 100; number <= 999; number++)
+            {
+                if (HasDifferentDigits(number))
+                {
+                    result += $"{number}, "; 
+                }
+            }
+
+            if (result.Length > 0)
+            {
+                result = result.Substring(0, result.Length - 2); 
+            }
+
+            Console.WriteLine(result); 
+            Console.ReadKey();
+        }
+
+        static bool HasDifferentDigits(int number)
+        {
+            bool[] digits = new bool[10];
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+
+                if (digits[digit])
+                {
+                    return false;
+                }
+
+                digits[digit] = true;
+                number /= 10;
+            }
+
+            return true;
+        }
+    }
+}
