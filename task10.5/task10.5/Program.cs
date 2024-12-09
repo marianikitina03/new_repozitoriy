9c3a07efa2b999f7b272a5fca37fc615ec2095af
maria <makwa4923@gmail.com>
2024-12-09 10:28:53 +0500

    ЗАДАНИЕ 10.5 выполнено

---
+++ task10.5/task10.5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число (факториал n!)");
+
+            long factorial;
+
+            if (!long.TryParse(Console.ReadLine(), out factorial) || factorial < 1)
+            {
+                Console.WriteLine("Введите натуральное число.");
+                Console.ReadKey();
+                return;
+            }
+
+            int n = 1;
+            long currentFactorial = 1;
+
+            while (currentFactorial < factorial)
+            {
+                n++;
+                currentFactorial *= n; 
+            }
+
+            if (currentFactorial == factorial)
+            {
+                Console.WriteLine($"Число n равно {n}");
+            }
+            else
+            {
+                Console.WriteLine($"{factorial} не является факториалом");
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+    
+}
